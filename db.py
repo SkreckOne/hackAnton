8cb1fe0e3057207778bc2c40bdb0b1bd222f5aa6
@diSp1rIt diSp1rIt <bisq1rit@gmail.com>
2022-08-06 17:42:18 +0200

    Init basic functions

--- db.py
+++ db.py
@@ -1,4 +1,7 @@
-async def is_exist(login: str):
+import datetime
+
+
+async def is_exist(login: str) -> bool:
     """
     Check for existing user (doesn't matter master or slave)
 
@@ -7,7 +10,7 @@ async def is_exist(login: str):
     """
 
 
-async def get_role(login: str):
+async def get_role(login: str) -> str:
     """
     Returns user role
 
@@ -15,3 +18,52 @@ async def get_role(login: str):
     :return 'master' or 'slave':
     """
 
+
+async def create_group(name: str) -> int:
+    """
+    Creates group
+
+    :param name:
+    :return group's id:
+    """
+
+
+async def set_salve_to_group(group_id: int, slave_id: int) -> None:
+    """
+    Sets slave to group
+
+    :param group_id:
+    :param slave_id:
+    :return None:
+    """
+
+
+async def create_task(name: str, date_start: datetime.date, date_end: datetime.date, time_start: datetime.time, time_end: datetime.time) -> int:
+    """
+    Create task
+
+    :param name:
+    :param date_start:
+    :param date_end:
+    :param time_start:
+    :param time_end:
+    :return task's id:
+    """
+
+
+async def delete_group(group_id: int) -> None:
+    """
+    Deletes group
+
+    :param group_id:
+    :return None:
+    """
+
+
+async def report_for_success(slave_id: int) -> None:
+    """
+    Sets flag tells system that slave completed him job
+
+    :param slave_id:
+    :return:
+    """
