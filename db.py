50a139ee9487b92702f89e015baaa5d0d0b32c27
@diSp1rIt diSp1rIt <bisq1rit@gmail.com>
2022-08-07 01:08:41 +0200

    fix bug

--- db.py
+++ db.py
@@ -12,10 +12,10 @@ def is_exist(login: str) -> bool:
 
 
 def get_user_id_by_login(login: str) -> int:
-    a = cur.execute(f"""SELECT id FROM master WHERE name = {login};""").fetchone()
+    a = cur.execute(f"""SELECT id FROM master WHERE name = '{login}';""").fetchone()
     if a is not None:
         return a
-    b = cur.execute(f"""SELECT id FROM slave WHERE name = {login};""").fetchone()
+    b = cur.execute(f"""SELECT id FROM slave WHERE name = '{login}';""").fetchone()
     return b
 
 
@@ -51,6 +51,16 @@ def create_task(text: str, date_start: datetime.date, date_end: datetime.date, t
     cur.execute(f'''
     INSERT INTO task (task_text, date_start, time_start, date_end, time_end, masterid)
     VALUES ('{text}', '{str(date_start)}', '{str(time_start)}', '{str(date_end)}', '{str(time_end)}', 2);''')
+
+
+def create_subtask(tsak_id: int, slvae_id: int) -> int:
+    """
+    Creates subtask for main task
+
+    :param tsak_id:
+    :param slvae_id:
+    :return:
+    """
 
 
 def delete_group(group_id: int) -> None:
